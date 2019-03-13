c49e90948b69a95e3fcc2d13f646bffd9169b37d
David DeAngelis <dj.deangelis@gmail.com>
2019-03-13 03:35:08 -0400

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import alex from 'alex'
+import reporter from 'vfile-reporter'
+
 import './App.css'
 
+const sampleText = 'With us on your side, you too can feel like a chairman of the board.  To boldly go where no man has gone before.  We pride ourselves on the workmanship of our products.  Damn, he\'ll love these prices.\nShe\'ll love that new phone!'
+
+const report = vfile => reporter([vfile], { defaultName: '-' })
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    const vfile = alex.text(sampleText)
+    this.state = {
+      diReport: report(vfile),
+      text: sampleText
+    }
+  }
+
+  onChange = ({ target: { value } }) => {
+    const vfile = alex.text(value)
+    this.setState({
+      diReport: report(vfile),
+      text: value
+    })
+  }
+
   render () {
+    const { diReport, text } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <pre>
+            <textarea style={{ fontSize: '14pt', width: '100%' }} onChange={this.onChange} value={text} />
+          </pre>
+        </div>
+        <div>
+          <pre>
+            <code>{diReport}</code>
+          </pre>
+        </div>
       </div>
     )
   }
